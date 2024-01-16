6865bfb720755037d295f0d0855d1356b1aa5ae2
sik2 <bickwang123@gmail.com>
2024-01-16 10:28:00 +0900

    secret key

--- src/test/java/com/example/jwt/JwtApplicationTests.java
+++ src/test/java/com/example/jwt/JwtApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.jwt;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class JwtApplicationTests {
 
+	@Value("${custom.jwt.secretKey}")
+	private String secretKey;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("시크릿 키 존재 여부 체크")
+	void test1() {
+		assertThat(secretKey).isNotNull();
 	}
 
 }
