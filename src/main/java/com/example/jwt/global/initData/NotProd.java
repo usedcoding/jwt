270087c22368ebf775fdf508db132ac9a99e30c8
usedcoding <ja14759@gmail.com>
2024-01-17 09:19:25 +0900

    17

--- src/main/java/com/example/jwt/global/initData/NotProd.java
+++ src/main/java/com/example/jwt/global/initData/NotProd.java
@@ -18,5 +18,6 @@ public class NotProd {
             Member member1 = memberService.join("user1", password, "user1@test.com");
             Member member2 = memberService.join("user2", password, "user2@test.com");
         };
+        //여기부터
     }
 }
